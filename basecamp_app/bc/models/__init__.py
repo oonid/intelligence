a6854f666c0abcfb6e32148ad483bced64325d53
oon arfiandwi <oon.arfiandwi@gmail.com>
2023-11-27 20:01:12 +0700

    add new model: BcComment. add bc migration file 0008.

--- basecamp_app/bc/models/__init__.py
+++ basecamp_app/bc/models/__init__.py
@@ -1,6 +1,7 @@
 from .company import *
 from .people import *
 from .project import *
+from .comment import *  # using people and project, set below both
 from .message import *  # using people and project, set below both
 from .questionnaire import *
 from .schedule import *  # using recurrence schedule, set below questionnaire
---
+++ basecamp_app/bc/models/comment.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from bc.models import BcPeople, BcProject
+
+
+class BcComment(models.Model):
+    """
+    https://github.com/basecamp/bc3-api/blob/master/sections/comments.md
+    """
+    id = models.BigIntegerField(primary_key=True)  # Basecamp Comment ID
+    status = models.CharField(max_length=30)
+    visible_to_clients = models.BooleanField()
+    created_at = models.DateTimeField(db_index=True)
+    updated_at = models.DateTimeField()
+    title = models.CharField(max_length=100)
+    inherits_status = models.BooleanField()
+    type = models.CharField(max_length=30)
+    url = models.URLField()
+    app_url = models.URLField()
+    bookmark_url = models.URLField()
+    # parent in ["Message", "Question::Answer", "Schedule::Entry", "Todolist", "Todo"]
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    # https://docs.djangoproject.com/en/4.2/ref/contrib/contenttypes/#generic-relations
+    parent = GenericForeignKey("content_type", "object_id")
+    bucket = models.ForeignKey(to=BcProject, on_delete=models.CASCADE)
+    creator = models.ForeignKey(to=BcPeople, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.type} {self.id} {self.title} ({self.status})'
