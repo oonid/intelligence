5cfa868d10f2dd3b490f32ef609ee980237bd4e8
oon arfiandwi <oon.arfiandwi@gmail.com>
2023-12-05 21:36:18 +0700

    add new serializer: BcWebhookSerializer, able to deserialize 16 type of endpoints, if unable to deserialize recording at least save webhook payload (raw).

---
+++ basecamp_app/bc/serializers/webhook.py
@@ -0,0 +1,275 @@
+from django.apps import apps as django_apps
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from bc.models import BcPeople, BcProject, BcWebhook
+from bc.serializers import BcPeopleSerializer
+from bc.apps import BcConfig
+
+from copy import deepcopy
+
+
+class BcWebhookSerializer(serializers.ModelSerializer):
+    creator = BcPeopleSerializer()
+    recording = None  # GenericForeignKey
+
+    def to_internal_value(self, data):
+        """https://www.django-rest-framework.org/api-guide/serializers/#to_internal_valueself-data
+        override this method because when overwrite method create, always get exists of creator
+        this method called before create, and this override make the creator can use existing
+        this method called before is_valid(), so no validated_data
+        """
+        # for debugging trails, save raw data (before changed below), make copy of data, not only refer to it
+        if 'raw' not in data:
+            data['raw'] = deepcopy(data)
+
+        # create or get creator instance from request data, remove (pop) the creator field from data
+        creator_data = data.pop('creator')
+
+        # can not use db_get_or_create_person as it would be circular import
+        try:
+            _creator = BcPeople.objects.get(id=creator_data["id"])
+        except BcPeople.DoesNotExist:
+            serializer = BcPeopleSerializer(data=creator_data)
+            if serializer.is_valid():
+                _creator = serializer.save()
+            else:  # invalid serializer
+                raise ValidationError({"get_or_create_person error": serializer.errors})
+
+        # if no exception, assign the creator to serializer
+        self.creator = _creator
+
+        # process recording
+        recording_data = data.pop('recording')
+
+        # process bucket inside recording
+        if not ('bucket' in recording_data and recording_data["bucket"]["type"] == "Project"):
+            raise ValidationError({"webhook recording bucket error":
+                                   f'webhook recording {recording_data["title"]} has no bucket type Project'})
+
+        # can not use utils as it would be circular import
+        _recording, _exception = self.get_webhook_recording(recording=recording_data)
+
+        # _recording can be undefined (as it still not saved on db), just skip the process, self.recording as null
+        if _recording:
+            # recording is a GenericForeignKey
+            self.recording = _recording
+
+        # even though the recording exception happened, we did not raise exception, as we just want to save the raw
+
+        return data  # return data without the creator field
+
+    class Meta:
+        model = BcWebhook
+        fields = '__all__'
+
+    def create(self, validated_data):
+        print(f'BcWebhookSerializer.create: {validated_data}')
+
+        if self.creator:
+            # recording could be None
+            payload = BcWebhook.objects.create(creator=self.creator, recording=self.recording, **validated_data)
+        else:  # undefined creator or recording
+            payload = BcWebhook.objects.create(**validated_data)
+        return payload
+
+    @staticmethod
+    def static_get_webhook_recording_types():
+        return ['Comment', 'Document', 'Message', 'Question::Answer', 'Schedule::Entry', 'Todo', 'Todolist', 'Upload',
+                'Vault', 'Client::Approval::Response', 'Client::Forward', 'Client::Reply', 'CloudFile',
+                'GoogleDocument', 'Inbox::Forward', 'Question']
+
+    @staticmethod
+    def static_get_webhook_recording_base_children_types():
+        return ["CloudFile", "GoogleDocument", "Inbox::Forward", "Client::Approval::Response", "Client::Forward",
+                "Client::Reply"]
+
+    @staticmethod
+    def static_get_webhook_recording_type_model_names():
+        return {
+            'CloudFile': 'BcCloudFile',
+            'GoogleDocument': 'BcGoogleDocument',
+            'Inbox::Forward': 'BcInboxForward',
+            'Client::Approval::Response': 'BcClientApprovalResponse',
+            'Client::Forward': 'BcClientForward',
+            'Client::Reply': 'BcClientReply',
+        }
+
+    @staticmethod
+    def static_get_type_model_names():
+        return {
+            'Comment': 'BcComment',
+            'Document': 'BcDocument',
+            'Message': 'BcMessage',
+            'Question': 'BcQuestion',
+            'Question::Answer': 'BcQuestionAnswer',
+            'Schedule::Entry': 'BcScheduleEntry',
+            'Todo': 'BcTodo',
+            'Todolist': 'BcTodolist',
+            'Upload': 'BcUpload',
+            'Vault': 'BcVault',
+        }
+
+    @staticmethod
+    def static_get_not_found_message(data):
+        return f'{data["type"]} {data["id"]} not found. unable to create new {data["type"]} from data: {data}'
+
+    def get_webhook_recording(self, recording):
+
+        type_model_names = self.static_get_type_model_names()
+
+        if recording["type"] in self.static_get_webhook_recording_base_children_types():
+
+            _recording, _exception = self.get_webhook_recording_base_child(recording=recording)
+            if not _recording:  # recording still not recorded on db, create webhook recording base child
+                _recording, _exception = self.create_webhook_recording_base_child(recording=recording)
+
+        elif recording["type"] in type_model_names.keys():
+
+            try:
+                app_config = django_apps.get_app_config(BcConfig.name)
+                app_model = django_apps.get_model(app_label=app_config.name,
+                                                  model_name=type_model_names[recording["type"]])
+                try:
+                    _recording = app_model.objects.get(id=recording["id"])
+                    _exception = None
+                except app_model.DoesNotExist:
+                    _recording = None
+                    _exception = self.static_get_not_found_message(recording)
+            except LookupError:
+                _recording = None
+                _exception = f'model not found for recording type {recording["type"]}'
+
+        else:
+            _recording = None
+            _exception = (f'recording {recording["id"]} type {recording["type"]} not in '
+                          f'{self.static_get_webhook_recording_types()}')
+
+        return _recording, _exception
+
+    def get_webhook_parent(self, parent):
+        if not parent:  # None
+            return None, f'webhook recording has no parent'
+
+        type_model_names = self.static_get_type_model_names()
+
+        if parent["type"] not in type_model_names.keys():
+            return None, f'webhook recording parent type unknown: {parent["type"]}'
+
+        try:
+            app_config = django_apps.get_app_config(BcConfig.name)
+            app_model = django_apps.get_model(app_label=app_config.name,
+                                              model_name=type_model_names[parent["type"]])
+            try:
+                _parent = app_model.objects.get(id=parent["id"])
+                _exception = None
+            except app_model.DoesNotExist:
+                _parent = None
+                _exception = self.static_get_not_found_message(parent)
+        except LookupError:
+            _parent = None
+            _exception = f'model not found for parent type {parent["type"]}'
+
+        return _parent, _exception
+
+    def db_get_recording_bucket(self, bucket):
+        if not bucket:  # None
+            return None, f'webhook recording has no bucket'
+
+        if not bucket["type"] == "Project":
+            return None, f'webhook recording has no bucket type Project'
+
+        try:
+            _bucket = BcProject.objects.get(id=bucket["id"])
+            _exception = None
+        except BcProject.DoesNotExist:
+            _bucket = None
+            _exception = self.static_get_not_found_message(data=bucket)
+
+        return _bucket, _exception
+
+    @staticmethod
+    def db_get_recording_creator(creator):
+        if not creator:  # None
+            return None, f'webhook recording has no creator'
+
+        # can not use db_get_or_create_person as it would be circular import
+        try:
+            _creator = BcPeople.objects.get(id=creator["id"])
+            _exception = None
+        except BcPeople.DoesNotExist:
+            serializer = BcPeopleSerializer(data=creator)
+            if serializer.is_valid():
+                _creator = serializer.save()
+                _exception = None
+            else:  # invalid serializer
+                _creator = None
+                _exception = f'get_or_create_person error: {serializer.errors}'
+
+        return _creator, _exception
+
+    def get_webhook_recording_base_child(self, recording):
+        type_model_names = self.static_get_webhook_recording_type_model_names()
+
+        if recording["type"] in self.static_get_webhook_recording_base_children_types():
+            try:
+                app_config = django_apps.get_app_config(BcConfig.name)
+                app_model = django_apps.get_model(app_label=app_config.name,
+                                                  model_name=type_model_names[recording["type"]])
+                try:
+                    _recording = app_model.objects.get(id=recording["id"])
+                    _exception = None
+                except app_model.DoesNotExist:
+                    _recording = None
+                    _exception = self.static_get_not_found_message(recording)
+            except LookupError:
+                _recording = None
+                _exception = f'model not found for recording type {recording["type"]}'
+
+        else:
+            _recording = None
+            _exception = (f'recording {recording["id"]} type {recording["type"]} not in '
+                          f'{self.static_get_webhook_recording_base_children_types()}')
+
+        return _recording, _exception
+
+    def create_webhook_recording_base_child(self, recording):
+        # to create a child of BcWebhookRecordingBase, it needs: bucket, creator, parent.
+
+        bucket_data = recording.pop('bucket', None)
+        _bucket, _exception = self.db_get_recording_bucket(bucket=bucket_data)
+        if not _bucket:
+            return None, _exception
+
+        # create or get creator instance from request data, remove (pop) the creator field from data
+        creator_data = recording.pop('creator', None)
+        _creator, _exception = self.db_get_recording_creator(creator=creator_data)
+        if not _creator:
+            return None, _exception
+
+        # process variant of parents
+        parent_data = recording.pop('parent', None)
+        _parent, _exception = self.get_webhook_parent(parent=parent_data)
+        if not _parent:
+            return None, _exception
+
+        type_model_names = self.static_get_webhook_recording_type_model_names()
+
+        if recording["type"] in self.static_get_webhook_recording_base_children_types():
+            try:
+                app_config = django_apps.get_app_config(BcConfig.name)
+                app_model = django_apps.get_model(app_label=app_config.name,
+                                                  model_name=type_model_names[recording["type"]])
+                _recording = app_model.objects.create(bucket=_bucket, creator=_creator, parent=_parent, **recording)
+                _exception = None
+            except LookupError:
+                _recording = None
+                _exception = f'model not found for recording type {recording["type"]}'
+
+        else:
+            _recording = None
+            _exception = (f'recording {recording["id"]} type {recording["type"]} not in '
+                          f'{self.static_get_webhook_recording_base_children_types()}')
+
+        return _recording, _exception
--- basecamp_app/bc/utils/const.py
+++ basecamp_app/bc/utils/const.py
@@ -14,9 +14,3 @@ def static_get_vault_parent_types():
 def static_get_recording_types():
     return ['Comment', 'Document', 'Message', 'Question::Answer', 'Schedule::Entry', 'Todo', 'Todolist', 'Upload',
             'Vault']
-
-
-def static_get_webhook_types():
-    return ['Comment', 'Document', 'Message', 'Question::Answer', 'Schedule::Entry', 'Todo', 'Todolist', 'Upload',
-            'Vault', 'Client::Approval::Response', 'Client::Forward', 'Client::Reply', 'CloudFile', 'GoogleDocument',
-            'Inbox::Forward', 'Question']
